97720f73e58e3fe2a4ec7785ea44e5ca52520693
TusharJain86 <tusharjain0806@gmail.com>
2025-10-25 09:43:11 +0530

    combined commit

---
+++ EmpPayroll/PayRollSystem.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class PayrollSystem {
+    private ArrayList<Employee> employeeList;
+
+    public PayrollSystem() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(int id) {
+        Employee employeeToRemove = null;
+        for (Employee employee : employeeList) {
+            if (employee.getId() == id) {
+                employeeToRemove = employee;
+                break;
+            }
+        }
+        
+        if (employeeToRemove != null) {
+            employeeList.remove(employeeToRemove);
+        }
+    }
+
+    public void displayAllEmployees() {
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
+
+    // ✅ FIXED: Implemented this method properly
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+}
